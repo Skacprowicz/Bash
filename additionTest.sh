023e8171a9118777da975219658b693dd956e9f8
SAM KACPROWICZ <samkacprowicz@Skaps-MacBook-Pro.local>
2017-11-08 22:28:53 -0700

    Addition

---
+++ additionTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SamSquanch $4m$kw4n(H
+# First Bash Program!
+# Sat Oct 29TH @ 12:07AM.  
+# Variables
+# num1
+# num2
+#Script Start
+read -p 'Choose a number!: ' num1 
+echo
+read -p 'Choose another number!: ' num2
+sleep 1
+echo
+echo "The sum of your two numbers is: $[ $num1 + $num2 ]"
